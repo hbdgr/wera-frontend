fa9435a79760806ee2eec761bf85fbd1dfd010a1
hbdgr <hbdgr@protonmail.com>
2024-07-15 22:35:59 +0200

    box for main component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,54 +3,62 @@ import TopButton from '../components/TopButton';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
 
-        <TopButton href="#" title="Button">
-          Button 1
-        </TopButton>
+      <div className="flex mb-[10px] items-center justify-center">
+        <a
+          className="flex items-center place-items-center pointer-events-none lg:pointer-events-auto"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/WeRaGlobal.svg"
+            alt="WeRa Global"
+            className="dark"
+            width={160}
+            height={60}
+            priority
+          />
+        </a>
+      </div>
+
+      <div className="w-full pb-[15px] max-w-4xl items-center justify-between font-mono text-sm lg:flex">
+
+        <div className="pb-1">
+          <TopButton href="#" title="Button">
+            Button 1
+          </TopButton>
+        </div>
 
         <TopButton href="#" title="Button">
           Button 2
         </TopButton>
 
-        <div className="flex items-center justify-center ">
-          <a
-            className="flex items-center place-items-center gap-2 pointer-events-none lg:pointer-events-auto"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/WeRaGlobal.svg"
-              alt="WeRa Global"
-              className="dark"
-              width={160}
-              height={60}
-              priority
-            />
-          </a>
-        </div>
+        <div className="w-0" />
 
         <TopButton href="#" title="Button">
           Button 3
         </TopButton>
 
-        <TopButton href="#" title="Button">
-          Button 4
-        </TopButton>
+        <div className="pb-1">
+          <TopButton href="#" title="Button">
+            Button 4
+          </TopButton>
+        </div>
 
       </div>
 
+      <div className="relative flex items-center justify-center w-[1100px] h-[700px]">
+        <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/distantSun.png')" }}></div>
 
-
-      <div className="flex items-center justify-center h-[600px]">
-        <div className="relative w-[600px] h-[400px] flex items-center justify-center">
-          <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: "url('/distantSun.png')" }}></div>
-          <div className="relative z-10 p-8 rounded-lg text-center">
-            <h1 className="text-2xl font-bold">Your Component</h1>
-            {/* Add more content here */}
-          </div>
+        <div className="
+          relative text-center w-[900px] h-[650px]
+          shadow-inner bg-black/80
+          rounded-xl border border-[#fdfd96]/60"
+        >
+          <h1 className="text-2xl font-bold"></h1>
         </div>
+
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-1/3 lg:max-w-5xl lg:grid-cols-2 lg:text-center">
--- src/components/TopButton.tsx
+++ src/components/TopButton.tsx
@@ -10,11 +10,20 @@ interface TopButtonProps {
 const TopButton: FC<TopButtonProps> = ({ href = "#", title = "Button", children }) => {
   return (
     <div className="relative inline-flex group">
-      <div className="absolute transition-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#fcffa4] via-[#fdfd96] to-[#fcffa4] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
+      <div className="
+        absolute transition-all duration-1000 opacity-70
+        bg-gradient-to-r from-[#fcffa4] via-[#fdfd96] to-[#fcffa4]
+        group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200
+        animate-tilt rounded-xl blur-md -inset-px
+        ">
       </div>
       <Link href={href} legacyBehavior>
         <a title={title}
-          className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+          className="
+            relative inline-flex items-center justify-center px-8 py-4
+            text-base font-bold text-white transition-all
+            duration-200 bg-gray-900 font-pj rounded-xl
+          "
           role="button">
           {children}
         </a>
